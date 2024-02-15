87334b64017a736da6a46c8e3cb8301518612c0e
Christopher Duggan <christopherduggan@gmail.com>
2024-02-15 11:56:18 -0500

    complete state lesson

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
+import { useState } from 'react'
+
 import logo from './logo.svg';
 import './App.css';
 import ToDoList from './ToDoList'
 
-const todos = [
-  'Have Fun',
-  'Learn React',
-  'Learn the MERN-Stack'
-]
-
 function App() {
+
+  const [showTodos, setShowTodos] = useState(true)
+
+  const [todos, setTodos] = useState([
+    'Have Fun',
+    'Learn React',
+    'Learn the MERN-Stack'
+  ])
+
+  // let x = 'buy bananas'
+
+
+function handleAddTodo(todo) {
+  // Create a new array that includes the new todo
+  const newTodos = [...todos, todo];
+  // Update state and re-render
+  setTodos(newTodos);
+}
+
   return (
     <div className="App">
       <h1>React To-Do</h1>
-      <ToDoList todos={todos} />
+
+      <button onClick={() => setShowTodos(!showTodos)}>Toggle Todo List</button>
+      {/* <button onClick={() => handleAddTodo(x)}> click me</button> */}
+
+      {showTodos && <ToDoList todos={todos} />}
+      {/* {todos.length > 0 ? <ToDoList todos={todos} /> : <p>there are no todos</p>} */}
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
